b5bb1e032a03238669214db54f016e95b21d47ef
Michelle Perez Morales <32468301+mhpm@users.noreply.github.com>
2026-08-12 15:19:44 -0600

    Move DbContext to Infrastructure

---
+++ Bookstore/src/BookStore.Infrastructure/Data/BookStoreDbContext.cs
@@ -0,0 +1,35 @@
+using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Data;
+
+public class BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books => Set<Book>();
+
+    public DbSet<Author> Authors => Set<Author>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.Name).IsRequired();
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+            entity.Property(b => b.Title).IsRequired();
+            entity.Property(b => b.ISBN).IsRequired();
+            entity.Property(b => b.PublishedDate).IsRequired();
+            entity.Property(b => b.Price).IsRequired();
+            entity.Property(b => b.Stock).IsRequired();
+
+            entity.HasOne<Author>()
+                .WithMany()
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}
